4d7654074db02e5db22b6bba19a4d9f278fc4a90
lemon <870317759@qq.com>
2020-05-28 15:39:24 +0800

    5.27 new push

---
+++ ch7/exercise7.10/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type str []byte
+
+func (s str) Len() int           { return len(s) }
+func (s str) Less(i, j int) bool { return s[i] < s[j] }
+func (s str) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func IsPalindrome(s sort.Interface) bool {
+	for i, j := 0, s.Len()-1; i < j; i, j = i+1, j-1 {
+		if s.Less(i, j) || s.Less(j, i) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a := str("abccba")
+	fmt.Println(IsPalindrome(a))
+
+	b := str("abcdefg")
+	fmt.Println(IsPalindrome(b))
+}
---
+++ ch7/exercise7.11/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	mux.Handle("/price", http.HandlerFunc(db.price))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+var mu sync.Mutex
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "params err: %s\n", err)
+	}
+	mu.Lock()
+	_, ok := db[item]
+	if ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s is exist\n", item)
+	} else {
+		db[item] = dollars(p)
+	}
+	mu.Unlock()
+}
+
+func (db database) read(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s isn't exist\n", item)
+	} else {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "params err: %s\n", err)
+	}
+	mu.Lock()
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf("%s isn'exist\n", item)
+	} else {
+		db[item] = dollars(p)
+	}
+	mu.Unlock()
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	mu.Lock()
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s isn't exist\n", item)
+	} else {
+		delete(db, item)
+	}
+	mu.Unlock()
+}
